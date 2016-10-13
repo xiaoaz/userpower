4e479af36b0af841e4b7e76be4c019eb901672d1
Administrator <616774757@qq.com>
2016-10-13 14:47:04 +0800

    修改后的spring启动可以

--- userpower-web/src/main/java/Startup.java
+++ userpower-web/src/main/java/Startup.java
@@ -1,9 +1,18 @@
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * Created by Administrator on 2016/10/13.
  */
-public class Startup {
+@SpringBootApplication
+@ComponentScan({"com.fc","com.hzit.controller","com.hzit.service"})
+@MapperScan("com.hzit.dao.mapper")
+public class Startup extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         System.out.println("启动类");
+
     }
 }
