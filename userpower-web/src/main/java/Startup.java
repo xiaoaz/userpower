ac3b55e06d5618537510b1f83b9f5f9f5a594c06
Administrator <616774757@qq.com>
2016-10-13 12:04:20 +0800

    Initial commit

---
+++ userpower-web/src/main/java/Startup.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+public class Startup {
+
+    public static void main(String[] args) {
+        System.out.println("启动类");
+    }
+}
---
+++ userpower-web/src/test/java/com/hzit/test/Test.java
@@ -0,0 +1,11 @@
+package com.hzit.test;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+public class Test {
+
+    public void test(){
+
+    }
+}
