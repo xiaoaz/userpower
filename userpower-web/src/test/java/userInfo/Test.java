61a268f402c2dde80a8f5c2a5e32e2e7e1730c9a
liuchao1026 <734362851@qq.com>
2016-10-14 16:03:54 +0800

    实体类

--- userpower-web/src/test/java/userInfo/Test.java
+++ userpower-web/src/test/java/userInfo/Test.java
@@ -1,7 +1,9 @@
 package userInfo;
 
 import com.hzit.Startapp;
+import com.hzit.dao.entity.Role;
 import com.hzit.dao.entity.Userinfo;
+import com.hzit.service.RolesService;
 import com.hzit.service.UserService;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +18,21 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class Test {
     @Autowired
     UserService userService;
+    @Autowired
+    RolesService rolesService;
+
+    /*
+    * userinfo的测试
+    * */
     @org.junit.Test
-    public void selectOne(){
+    public void selectOneuser(){
         Userinfo userinfo=userService.selectOne("诸葛亮", "123");
         System.out.println(userinfo.getUsername());
+    }
+
+    @org.junit.Test
+    public void selctOnerole(){
+        Role role=rolesService.selectOne(1);
+        System.out.println(role.getRoleName());
     }
 }
